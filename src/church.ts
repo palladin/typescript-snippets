522c0b076222e6150cb615118317837f2b34db1d
Nick Palladinos <nick.palladinos@gmail.com>
2022-06-23 19:46:38 +0300

    Update church

--- src/church.ts
+++ src/church.ts
@@ -5,9 +5,14 @@ const toNum: (n: Church) => number = n => n<number>(x => 1 + x)(0)
 
 const zero: Church = f => x => x
 const succ : (n: Church) => Church = n => f => x => f(n(f)(x))
+const add: (m: Church) => (n: Church) => Church = m => n => f => x => m(f)(n(f)(x))
+const mult: (m: Church) => (n: Church) => Church = m => n => f => x => m(n(f))(x)
+const exp: (m: Church) => (n: Church) => Church = m => n => n(m)
 
 const one = succ(zero)
 const two = succ(one)
 const three = succ(two) 
 
-console.log(toNum(three))
+console.log(toNum(add(three)(two)))
+console.log(toNum(mult(three)(two)))
+console.log(toNum(exp(three)(two)))
