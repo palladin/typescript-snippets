5a1ab701b5d0a99b4949d69446dadc11a60b50fd
Nick Palladinos <nick.palladinos@gmail.com>
2022-07-04 16:26:57 +0300

    Add cont

--- src/church.ts
+++ src/church.ts
@@ -1,18 +1,20 @@
+module Snippet4 {
 
-type Church = <T>(f: (x: T) => T) => (x: T) => T
+    type Church = <T>(f: (x: T) => T) => (x: T) => T
 
-const toNum: (n: Church) => number = n => n<number>(x => 1 + x)(0)
+    const toNum: (n: Church) => number = n => n<number>(x => 1 + x)(0)
 
-const zero: Church = f => x => x
-const succ : (n: Church) => Church = n => f => x => f(n(f)(x))
-const add: (m: Church) => (n: Church) => Church = m => n => f => x => m(f)(n(f)(x))
-const mult: (m: Church) => (n: Church) => Church = m => n => f => x => m(n(f))(x)
-const exp: (m: Church) => (n: Church) => Church = m => n => n(m)
+    const zero: Church = f => x => x
+    const succ : (n: Church) => Church = n => f => x => f(n(f)(x))
+    const add: (m: Church) => (n: Church) => Church = m => n => f => x => m(f)(n(f)(x))
+    const mult: (m: Church) => (n: Church) => Church = m => n => f => x => m(n(f))(x)
+    const exp: (m: Church) => (n: Church) => Church = m => n => n(m)
 
-const one = succ(zero)
-const two = succ(one)
-const three = succ(two) 
+    const one = succ(zero)
+    const two = succ(one)
+    const three = succ(two) 
 
-console.log(toNum(add(three)(two)))
-console.log(toNum(mult(three)(two)))
-console.log(toNum(exp(three)(two)))
+    console.log(toNum(add(three)(two)))
+    console.log(toNum(mult(three)(two)))
+    console.log(toNum(exp(three)(two)))
+}
---
+++ src/cont.ts
@@ -0,0 +1,28 @@
+module Snippet5 {
+
+    type Cont<T, R> = (k: (x: T) => R) => R
+    type ContMonad<T, R> = 
+        { 
+            value: Cont<T, R>, 
+            then: <U>(f: (x: T) => ContMonad<U, R>) => ContMonad<U, R>
+        }
+
+
+    function begin<T, R>(cont: Cont<T, R>): ContMonad<T, R> {
+
+        const pure: <T, R>(x: T) => Cont<T, R> = x => k => k(x)
+        const bind: <T, U, R>(m: Cont<T, R>) => (f: (x: T) => Cont<U, R>) => Cont<U, R> = 
+            m => f => k => m(x => f(x)(k))
+
+        return {
+            value: cont,
+            then: <U>(f: (x: T) => ContMonad<U, R>) => begin<U, R>(bind<T, U, R>(cont)(x => f(x).value))
+        }
+    }
+
+    function delay<T>(v: T, d: number): ContMonad<T, void> {
+        return begin(k => setTimeout(() => k(v), d))
+    }
+
+    delay(42, 5000).value(x => console.log(x))
+}
